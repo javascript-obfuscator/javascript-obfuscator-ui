39ef2dbf0a67f86fe586b970c0e930572502640e
Tiago Serafim <tserafim@gmail.com>
2016-10-28 10:52:11 -0200

    Options are now persisted to the localStorage.

--- App/index.js
+++ App/index.js
@@ -8,6 +8,8 @@ import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 import promiseMiddleware from 'redux-promise-middleware';
 
+import { loadState, saveState } from './localStorage';
+
 import reducer from './reducers'
 
 import App from './containers/App'
@@ -20,10 +22,21 @@ if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger());
 }
 
+const persistedState = loadState();
+
 const store = createStore(
   reducer,
+  persistedState,
   applyMiddleware(...middleware)
 );
+
+// There's no need to throttle the saveState function because
+// we don't change the state very often
+store.subscribe(() => {
+  saveState({
+    options: store.getState().options
+  });
+})
 
 render(
     <Provider store={store}>
---
+++ App/localStorage.js
@@ -0,0 +1,22 @@
+// from https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    // ignore
+  }
+};
