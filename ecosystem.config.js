7e7e7f9913d73d631b1b2742c1efe5f8048cd828
sanex3339 <yarabotayuvyandex3339>
2019-11-22 01:09:41 +0300

    Fixed pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
+const {resolve} = require('path');
+
 module.exports = {
-  apps : [{
-    name: 'javascript-obfuscator-ui',
-    script: 'server.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G'
-  }]
+    apps : [{
+        name: 'javascript-obfuscator-ui',
+        script: resolve(__dirname, './server'),
+        instances: 1,
+        merge_logs: true,
+        out_file: '/var/log/javascript-obfuscator-ui/app.log',
+        error_file: '/var/log/javascript-obfuscator-ui/error.log',
+    }]
 };
